57c402ce1ad98a59d39884cac97cba5d8614d1c7
Akzam Khamitov <makoron3002@gmail.com>
2023-03-24 01:14:47 +0600

    Created Card.module.scss and renamed Card.js to index.js, put it all in one folder with the name Card

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import Card from "./components/Card";
 import Drawer from "./components/Drawer";
 import Header from "./components/Header";
 
+const sneakers = [
+	{ title: 'Men\'s sneakers Nike Blazer Mid Suede', price: '300', image: '1.jpg' },
+	{ title: 'Men\'s sneakers Nike Air Max 270', price: '350', image: '2.jpg' }
+]
+
 function App() {
 	return (
 		<div className="wrapper clear">
@@ -14,12 +19,20 @@ function App() {
 					<h1>All sneakers</h1>
 					<div className="search-block d-flex">
 						<img src="/img/search.svg" alt="Search" />
-						<input placeholder="Seaech..." />
+						<input place holder="Seaech..." />
 					</div>
 				</div>
 
 				<div className="d-flex">
-					<Card />
+					{
+						sneakers.map(s =>
+							<Card
+								title={s.title}
+								price={s.price}
+								imageUrl={s.image}
+							/>
+						)
+					}
 				</div>
 			</div>
 		</div>
